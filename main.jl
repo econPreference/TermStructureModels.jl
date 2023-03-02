a357cf073a92134f89919c2d4939bcd007e10c7b
Preference <econ.preference@gmail.com>
2023-03-02 11:21:12 +0900

    simplify main.jl

--- main.jl
+++ main.jl
@@ -3,33 +3,36 @@ using GDTSM, RCall, CSV, DataFrames, Dates, Plots
 date_start = Date("1987-01-01", "yyyy-mm-dd")
 date_end = Date("2020-02-01", "yyyy-mm-dd")
 
-## Data: macro data
-R"library(fbi)"
-raw_fred = rcopy(rcall(:fredmd, file="/Users/preference/Dropbox/code/Julia/GDTSM/current.csv", date_start=date_start, date_end=date_end, transform=true))
-macros = raw_fred[:, [1; (1+1):(1+57); (1+59):(1+83); (1+101):size(raw_fred, 2)]]
-# selected_macros = [:DPCERA3M086SBEA, :INDPRO, :IPFINAL, :PAYEMS, :MANEMP, :CE16OV, :UNRATE, :HOUST, :PERMIT, :CPIAUCSL, :M2REAL, Symbol("S&P 500"), :TOTRESNS]
-# macros = macros[:, selected_macros]
+begin ## Data: macro data
+    R"library(fbi)"
+    raw_fred = rcopy(rcall(:fredmd, file="/Users/preference/Dropbox/code/Julia/GDTSM/current.csv", date_start=date_start, date_end=date_end, transform=true))
+    macros = raw_fred[:, [1; (1+1):(1+57); (1+59):(1+83); (1+101):size(raw_fred, 2)]]
+    selected_macros = [:DPCERA3M086SBEA, :INDPRO, :IPFINAL, :PAYEMS, :MANEMP, :CE16OV, :UNRATE, :HOUST, :PERMIT, :CPIAUCSL, :M2REAL, Symbol("S&P 500"), :TOTRESNS]
+    macros = macros[:, selected_macros]
+end
 
-## Data: yield data
-# yield(3 months) and yield(6 months)
-raw_fred = rcopy(rcall(:fredmd, file="/Users/preference/Dropbox/code/Julia/GDTSM/current.csv", date_start=date_start, date_end=date_end, transform=false))
-Y3M = raw_fred[:, :TB3MS]
-Y6M = raw_fred[:, :TB6MS]
-# longer than one year
-raw_yield = CSV.File("feds200628.csv", missingstring="NA", types=[Date; fill(Float64, 99)]) |> DataFrame |> (x -> [x[8:end, 1] x[8:end, 69:78]]) |> dropmissing
-idx = month.(raw_yield[:, 1]) |> x -> (x .!= [x[2:end]; x[end]])
-yield_year = raw_yield[idx, :]
-yield_year = yield_year[findall(x -> x == yearmonth(date_start), yearmonth.(yield_year[:, 1]))[1]:findall(x -> x == yearmonth(date_end), yearmonth.(yield_year[:, 1]))[1], :]
-yields = DataFrame([yield_year[:, 1] Y3M Y6M Matrix(yield_year[:, 2:end])], [:date, :M3, :M6, :Y1, :Y2, :Y3, :Y4, :Y5, :Y6, :Y7, :Y8, :Y9, :Y10])
-
+begin ## Data: yield data
+    # yield(3 months) and yield(6 months)
+    raw_fred = rcopy(rcall(:fredmd, file="/Users/preference/Dropbox/code/Julia/GDTSM/current.csv", date_start=date_start, date_end=date_end, transform=false))
+    Y3M = raw_fred[:, :TB3MS]
+    Y6M = raw_fred[:, :TB6MS]
+    # longer than one year
+    raw_yield = CSV.File("feds200628.csv", missingstring="NA", types=[Date; fill(Float64, 99)]) |> DataFrame |> (x -> [x[8:end, 1] x[8:end, 69:78]]) |> dropmissing
+    idx = month.(raw_yield[:, 1]) |> x -> (x .!= [x[2:end]; x[end]])
+    yield_year = raw_yield[idx, :]
+    yield_year = yield_year[findall(x -> x == yearmonth(date_start), yearmonth.(yield_year[:, 1]))[1]:findall(x -> x == yearmonth(date_end), yearmonth.(yield_year[:, 1]))[1], :]
+    yields = DataFrame([yield_year[:, 1] Y3M Y6M Matrix(yield_year[:, 2:end])], [:date, :M3, :M6, :Y1, :Y2, :Y3, :Y4, :Y5, :Y6, :Y7, :Y8, :Y9, :Y10])
+end
 ## Tuning hyper-parameters
 
-ρ = Vector{Float64}(undef, size(macros[:, 2:end], 2))
-for i in eachindex(ρ)
-    if rcopy(rcall(:describe_md, names(macros[:, 2:end])))[:, :tcode][i] ∈ ["1", "4"]
-        ρ[i] = 0.9
-    else
-        ρ[i] = 0
+begin
+    ρ = Vector{Float64}(undef, size(macros[:, 2:end], 2))
+    for i in eachindex(ρ)
+        if rcopy(rcall(:describe_md, names(macros[:, 2:end])))[:, :tcode][i] ∈ ["1", "4"]
+            ρ[i] = 0.9
+        else
+            ρ[i] = 0
+        end
     end
 end
 tuned = tuning_hyperparameter(Array(yields[:, 2:end]), Array(macros[:, 2:end]), ρ)
