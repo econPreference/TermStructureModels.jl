ca3c702872a8767f01b270e3761758ee8a7500f5
Preference <econ.preference@gmail.com>
2023-03-06 14:43:37 +0900

    Modifying report table in inference.jl

--- parallel.jl
+++ parallel.jl
@@ -9,7 +9,8 @@ addprocs(n_core)
     Pkg.precompile()
 end
 @everywhere begin
-    using GDTSM, BlackBoxOptim
+    using GDTSM, BlackBoxOptim, Random
+    Random.seed!(1234)
 end
 using RCall, CSV, DataFrames, Dates, Plots
 date_start = Date("1986-12-01", "yyyy-mm-dd")
