f4ec00b2e713e7b9817b4ca93e2fd06add4184fc
Preference <econ.preference@gmail.com>
2023-03-23 07:43:23 +0900

    fix bugs

--- parallel.jl
+++ parallel.jl
@@ -27,7 +27,7 @@ begin ## Data: macro data
     end
     macros = macros[:, idx]
     excluded = ["W875RX1", "IPFPNSS", "IPFINAL", "IPCONGD", "IPDCONGD", "IPNCONGD", "IPBUSEQ", "IPMAT", "IPDMAT", "IPNMAT", "IPMANSICS", "IPB51222S", "IPFUELS", "HWIURATIO", "CLF16OV", "CE16OV", "UEMPLT5", "UEMP5TO14", "UEMP15OV", "UEMP15T26", "UEMP27OV", "USGOOD", "CES1021000001", "USCONS", "MANEMP", "DMANEMP", "NDMANEMP", "SRVPRD", "USTPU", "USWTRADE", "USTRADE", "USFIRE", "USGOVT", "AWOTMAN", "AWHMAN", "CES2000000008", "CES3000000008", "HOUSTNE", "HOUSTMW", "HOUSTS", "HOUSTW", "PERMITNE", "PERMITMW", "PERMITS", "PERMITW", "NONBORRES", "DTCOLNVHFNM", "AAAFFM", "BAAFFM", "EXSZUSx", "EXJPUSx", "EXUSUKx", "EXCAUSx", "WPSFD49502", "WPSID61", "WPSID62", "CPIAPPSL", "CPITRNSL", "CPIMEDSL", "CUSR0000SAC", "CUSR0000SAS", "CPIULFSL", "CUSR0000SA0L2", "CUSR0000SA0L5", "DDURRG3M086SBEA", "DNDGRG3M086SBEA", "DSERRG3M086SBEA"]
-    push!(excluded, "CMRMTSPLx", "RETAILx", "HWI", "UEMPMEAN", "CLAIMSx", "AMDMNOx", "ANDENOx", "AMDMUOx", "BUSINVx", "ISRATIOx", "BUSLOANS", "NONREVSL", "CONSPI", "S&P: indust", "S&P div yield", "S&P PE ratio", "M1SL", "BOGMBASE", "RPI", "UMCSENTx", "VIXCLSx")
+    push!(excluded, "CMRMTSPLx", "RETAILx", "HWI", "UEMPMEAN", "CLAIMSx", "AMDMNOx", "ANDENOx", "AMDMUOx", "BUSINVx", "ISRATIOx", "BUSLOANS", "NONREVSL", "CONSPI", "S&P: indust", "S&P div yield", "S&P PE ratio", "M1SL", "BOGMBASE", "UMCSENTx", "VIXCLSx")
     macros = macros[:, findall(x -> !(x ∈ excluded), names(macros))]
     ρ = Vector{Float64}(undef, size(macros[:, 2:end], 2))
     for i in axes(macros[:, 2:end], 2) # i'th macro variable (excluding date)
@@ -100,7 +100,7 @@ end
 saved_TP = [par_TP[i][1] for i in eachindex(par_TP)]
 save("TP.jld2", "TP", saved_TP)
 saved_TP = load("TP.jld2")["TP"]
-saved_Xθ = latentspace(saved_θ, Array(yields[:, 2:end]), τₙ)
-fitted = fitted_YieldCurve([1; τₙ], saved_Xθ)
-plot(mean(fitted)[:yields][tuned.p+1:end, 1])
-plot!(mean(fitted)[:yields][tuned.p+1:end, end] - mean(saved_TP)[:TP])
+# saved_Xθ = latentspace(saved_θ, Array(yields[:, 2:end]), τₙ)
+# fitted = fitted_YieldCurve([1; τₙ], saved_Xθ)
+# plot(mean(fitted)[:yields][tuned.p+1:end, 1])
+# plot!(mean(fitted)[:yields][tuned.p+1:end, end] - mean(saved_TP)[:TP])
