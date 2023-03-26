5c00c63a9781f0c1605f243908f00a72fa496a95
Preference <econ.preference@gmail.com>
2023-03-26 14:07:49 +0900

    fix bugs

--- parallel.jl
+++ parallel.jl
@@ -64,7 +64,7 @@ end
 
 ## Tuning hyper-parameters
 τₙ = [3; 6; collect(12:12:120)]
-tuned = tuning_hyperparameter(Array(yields[:, 2:end]), Array(macros[:, 2:end]), τₙ, ρ; mSR_median=1.5, mSR_tail=3.0, upper_lag=6, upper_q=[0.0001, 100]) # the largest search space given mSR and ΩFF
+tuned = tuning_hyperparameter(Array(yields[:, 2:end]), Array(macros[:, 2:end]), τₙ, ρ; mSR_median=1.5, mSR_tail=3.0, upper_lag=6, upper_q1=1) # the largest search space given mSR and ΩFF
 save("tuned.jld2", "tuned", tuned)
 tuned = load("tuned.jld2")["tuned"]
 # mSR = maximum_SR(Array(yields[:, 2:end]), Array(macros[:, 2:end]), tuned, τₙ, ρ; iteration =1000)
