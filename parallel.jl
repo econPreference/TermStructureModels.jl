a55120fcac7e322cf40e731cc4ed260676fa9fd2
Preference <econ.preference@gmail.com>
2023-04-04 15:47:27 +0900

    changes in defaults

--- parallel.jl
+++ parallel.jl
@@ -64,7 +64,7 @@ end
 
 ## Tuning hyper-parameters
 τₙ = [3; 6; collect(12:12:120)]
-tuned = tuning_hyperparameter(Array(yields[:, 2:end]), Array(macros[:, 2:end]), τₙ, ρ; mSR_tail=2.0, mSR_median=0.75, upper_lag=6, upper_q1=0.005, upper_q45=0.01, σ²kQ_infty=0.01^2)
+tuned = tuning_hyperparameter(Array(yields[:, 2:end]), Array(macros[:, 2:end]), τₙ, ρ; mSR_tail=2.0, mSR_median=0.75, upper_lag=6, upper_q1=0.001, upper_q45=0.01, σ²kQ_infty=0.01^2)
 save("tuned.jld2", "tuned", tuned)
 tuned = load("tuned.jld2")["tuned"]
 mSR_prior = maximum_SR(Array(yields[:, 2:end]), Array(macros[:, 2:end]), tuned, τₙ, ρ; iteration=1000)
