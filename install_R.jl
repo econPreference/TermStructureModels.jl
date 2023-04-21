86070df1e42894b1dfa2f6a7f50d1b83b3d4ba81
Preference <econ.preference@gmail.com>
2023-04-22 00:36:25 +0900

    add contents for windows users

--- install_R.jl
+++ install_R.jl
@@ -5,11 +5,12 @@
 # 4. In Juila, run  " ENV["R_HOME"]="...the address in step 2..." "
 # 5. In Juila, run " using Pkg " and " Pkg.add("RCall") "
 using Conda, Pkg
-ENV["R_HOME"] = "/Library/Frameworks/R.framework/Resources"
+# ENV["R_HOME"] = "/Library/Frameworks/R.framework/Resources" # For my mac computer
+ENV["R_HOME"] = "C:/Program Files/R/R-4.2.3" # For my windows computer
 Pkg.build("RCall")
 using RCall
-R"install.packages('MASS', repos='https://cloud.r-project.org/')"
-R"install.packages('qgraph', repos='https://cloud.r-project.org/')"
-R"install.packages('GIGrvg', repos='https://cloud.r-project.org/')"
+R"install.packages('MASS', repos='https://cloud.r-project.org/', type = 'binary')"
+R"install.packages('qgraph', repos='https://cloud.r-project.org/', type = 'binary')"
+R"install.packages('GIGrvg', repos='https://cloud.r-project.org/', type = 'binary')"
 println("All R packages are installed.")
 ######################
