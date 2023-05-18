af769f76ef2bb151ce72f6b33bbc83861a525a9a
Preference <econ.preference@gmail.com>
2023-05-19 08:30:56 +0900

    σ²kQ_infty

--- parallel.jl
+++ parallel.jl
@@ -24,7 +24,7 @@ step = 3
 upper_q1 = 1
 upper_q4 = 1
 upper_q5 = 1
-μkQ_infty = -0.02
+μkQ_infty = -0.08
 mSR_tail = 4.0
 
 lag = 1
@@ -84,7 +84,7 @@ begin ## Data: yield data
     yields = yields[3:end, :]
 end
 
-#mean_TP([0.06, 0, 0], 120, Array(yields[p_max-lag+1:end, 2:end]), τₙ, lag; κQ=0.0609)
+# mean_TP([0.08, 0, 0], 120, Array(yields[p_max-lag+1:end, 2:end]), τₙ, lag; κQ=0.0609)
 
 if step == 0 ## Drawing pareto frontier
 
