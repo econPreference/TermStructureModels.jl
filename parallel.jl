12bc531257c5f08e3f9b94943eb49b5c82979109
Preference <econ.preference@gmail.com>
2023-06-08 22:52:09 +0900

    crowd out the parallel

--- parallel.jl
+++ parallel.jl
@@ -149,17 +149,35 @@ elseif step == 2 ## Estimation
         saved_θ = saved_θ[burnin+1:end]
         iteration = length(saved_θ)
 
-        saved_θ, accept_rate = stationary_θ(saved_θ)
+        par_stationary_θ = @showprogress 1 "Stationary filtering..." pmap(1:iteration) do i
+            stationary_θ([saved_θ[i]])
+        end
+        saved_θ = Vector{Parameter}(undef, 0)
+        for i in eachindex(par_stationary_θ)
+            if !isempty(par_stationary_θ[i][1])
+                push!(saved_θ, par_stationary_θ[i][1][1])
+            end
+        end
+        accept_rate = [par_stationary_θ[i][2] / 100 for i in eachindex(par_stationary_θ)] |> sum |> x -> (100x / iteration)
+        iteration = length(saved_θ)
         save("posterior.jld2", "samples", saved_θ, "acceptPr", [acceptPr_C_σ²FF; acceptPr_ηψ], "accept_rate", accept_rate)
     else
         saved_θ = load("posterior.jld2")["samples"]
         iteration = length(saved_θ)
-        saved_θ, trace_sparsity = sparse_precision(saved_θ, size(macros, 1) - tuned.p)
+
+        par_sparse_θ = @showprogress 1 "Sparse precision..." pmap(1:iteration) do i
+            sparse_precision([saved_θ[i]], size(macros, 1) - tuned.p)
+        end
+        saved_θ = [par_sparse_θ[i][1][1] for i in eachindex(par_sparse_θ)]
+        trace_sparsity = [par_sparse_θ[i][2][1] for i in eachindex(par_sparse_θ)]
         save("sparse.jld2", "samples", saved_θ, "sparsity", trace_sparsity)
     end
 
     if is_TP
-        saved_TP = term_premium(TPτ_interest, τₙ, saved_θ, Array(yields[p_max-lag+1:end, 2:end]), Array(macros[p_max-lag+1:end, 2:end]))
+        par_TP = @showprogress 1 "Term premium..." pmap(1:iteration) do i
+            term_premium(TPτ_interest, τₙ, [saved_θ[i]], Array(yields[p_max-lag+1:end, 2:end]), Array(macros[p_max-lag+1:end, 2:end]))
+        end
+        saved_TP = [par_TP[i][1] for i in eachindex(par_TP)]
         save("TP.jld2", "TP", saved_TP)
     end
 
