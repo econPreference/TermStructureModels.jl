693fed905eb9dbe5ca4e7e55d5068c15039c3757
Preference <econ.preference@gmail.com>
2023-06-09 18:54:01 +0900

    rescaling PCs using std of yields

--- src/Theoretical.jl
+++ src/Theoretical.jl
@@ -434,8 +434,7 @@ function PCA(yields, p, proxies=[]; rescaling=true)
         return Matrix(PCs), Matrix(OCs), Wₚ, Wₒ, mean_PCs[1, :]
     else
         ## rescaling
-        # mean_std = mean(std(yields[(p+1):end, :], dims=1))
-        mean_std = 1
+        mean_std = mean(std(yields[(p+1):end, :], dims=1))
         scale_PCs = mean_std ./ std(PCs, dims=1)'
         scale_OCs = mean_std ./ std(OCs, dims=1)'
 
