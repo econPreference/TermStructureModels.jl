ab2347c5fa470726653dce2ba37e7b696b481962
Preference <econ.preference@gmail.com>
2023-07-14 12:52:13 +0900

    fix the setting

--- ex_scenario.jl
+++ ex_scenario.jl
@@ -21,17 +21,5 @@ par_prediction = @showprogress 1 "Scenario..." pmap(1:ceil(Int, maximum(ineff)):
     scenario_sampler(scene, [24, 120], 10, [saved_θ[i]], Array(yields[sdate(1987, 1):sdate(2020, 2), 2:end]), Array(macros[sdate(1987, 1):sdate(2020, 2), 2:end]), τₙ; mean_macros)
 end
 
-# saved_prediction = [par_prediction[i][1] for i in eachindex(par_prediction)]
-saved_prediction = Vector{Forecast}(undef, length(par_prediction))
-for i in eachindex(saved_prediction)
-    predicted_factors = deepcopy(par_prediction[i][1][:factors])
-    for j in 1:dP-dQ
-        if idx_diff[j] == 2
-            predicted_factors[1, dQ+j] = macros_growth[sdate(2020, 3), j]
-            predicted_factors[:, dQ+j] = predicted_factors[:, dQ+j] |> cumsum
-        end
-    end
-    saved_prediction[i] = Forecast(yields=par_prediction[i][1][:yields], factors=predicted_factors, TP=par_prediction[i][1][:TP])
-end
-
+saved_prediction = [par_prediction[i][1] for i in eachindex(par_prediction)]
 save("scenario.jld2", "forecasts", saved_prediction)
