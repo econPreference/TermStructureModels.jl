34cb9f37eb85f59c16e5d084bc191f2115c0041c
Preference <econ.preference@gmail.com>
2024-06-21 16:26:25 +0900

    add some diagnostics

--- simulation.jl
+++ simulation.jl
@@ -3,7 +3,7 @@ using Pkg, Revise
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
 Pkg.precompile()
-using TermStructureModels, ProgressMeter, Distributions, LinearAlgebra, Distributions
+using TermStructureModels, ProgressMeter, Distributions, LinearAlgebra, Distributions, Plots
 
 ## Simulating sample data
 T = 1000
@@ -33,10 +33,11 @@ yields, latents, macros = generative(T, dP, tau_n, p, 0.01; kappaQ, kQ_infty, KP
 diag_G = diag_G[dimQ()+1:end]
 rho = zeros(dP - dimQ())
 rho[diag_G.>0.5] .= 1.0
-medium_tau = collect(30:2:48)
+medium_tau = collect(24:3:60)
 medium_tau_pr = [truncated(Normal(1, 0.1), -1, 1), truncated(Normal(1, 0.1), -1, 1), truncated(Normal(1, 0.1), -1, 1)]
 std_kQ_infty = 0.2
 tuned, opt = tuning_hyperparameter(yields, macros, tau_n, rho; std_kQ_infty, medium_tau, medium_tau_pr)
+p = tuned.p
 
 ## Estimating
 iteration = 10_000
@@ -48,5 +49,82 @@ saved_Xθ = latentspace(saved_θ, yields, tau_n)
 saved_TP = term_premium(120, tau_n, saved_θ[1:50:end], yields, macros)
 reduced_θ = reducedform(saved_θ, yields, macros, tau_n)
 fits = fitted_YieldCurve(tau_n, saved_Xθ)
+
 idx = 18
-cor([mean(fits)[:yields][:, idx] yields[:, idx]])
+plot(mean(fits)[:yields][:, idx])
+plot!(yields[:, idx])
+
+data = [yields macros]
+factors = [PCA(yields, p)[1] macros]
+# Compare observations and fitted variables of the reducedform transition equation
+fitted_samples = Vector{Matrix}(undef, length(reduced_θ))
+@showprogress for iter in eachindex(reduced_θ)
+    KPF = reduced_θ[iter].KPF
+    GPFF = reduced_θ[iter].GPFF
+    OmegaFF = reduced_θ[iter].OmegaFF
+
+    fitted = zeros(size(PCs, 1), dP)
+    fitted[1:p, :] = factors[1:p, :]
+    for t in p+1:size(factors, 1)
+        Xs = factors[t-1:-1:t-p, :]' |> vec
+        fitted[t, :] = KPF + GPFF * Xs + rand(MvNormal(zeros(dP), OmegaFF))
+    end
+    fitted_samples[iter] = fitted
+end
+idx = 3
+plot(factors[:, idx])
+plot!(mean(fitted_samples)[:, idx])
+
+## Diagnostics by the scenario analysis
+predicted_yields = zeros(size(yields, 1), length(tau_n))
+predicted_factors = zeros(size(yields, 1), dP)
+for t = (size(yields, 1)-10):(size(yields, 1)-1)
+    prediction = conditional_forecasts([], 120, 1, saved_θ, yields[1:t, :], macros[1:t, :], tau_n)
+    predicted_yields[t+1, :] = mean(prediction)[:yields][end, :]
+    predicted_factors[t+1, :] = mean(prediction)[:factors][end, :]
+end
+idx = 4
+plot(factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+idx = 18
+plot(yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+
+predicted_yields = zeros(size(yields, 1), length(tau_n))
+predicted_factors = zeros(size(yields, 1), dP)
+for t = (size(yields, 1)-10):(size(yields, 1)-1)
+
+    S = zeros(1, dP - dimQ() + length(tau_n))
+    S[1, 19] = 1.0
+    s = [data[t+1, 19]]
+    scene = Scenario(combinations=deepcopy(S), values=deepcopy(s))
+
+    prediction = conditional_forecasts([scene], 120, 1, saved_θ, yields[1:t, :], macros[1:t, :], tau_n)
+    predicted_yields[t+1, :] = mean(prediction)[:yields][end, :]
+    predicted_factors[t+1, :] = mean(prediction)[:factors][end, :]
+end
+idx = 4
+plot(factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+idx = 18
+plot(yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+
+scene = Vector{Scenario}(undef, 4)
+S = zeros(2, dP - dimQ() + length(tau_n))
+s = zeros(2)
+for h = 1:4
+    S[1, 1] = 1.0
+    s[1] = data[size(data, 1)-4+h, 1]
+    S[2, 19] = 1.0
+    s[2] = data[size(data, 1)-4+h, 19]
+    scene[h] = Scenario(combinations=deepcopy(S), values=deepcopy(s))
+end
+prediction = conditional_forecasts(scene, 120, 8, saved_θ, yields[1:size(data, 1)-4, :], macros[1:size(data, 1)-4, :], tau_n)
+plot(mean(prediction)[:TP])
+idx = 4
+plot(factors[(end-3):end, idx])
+plot!(mean(prediction)[:factors][:, idx])
+idx = 18
+plot(yields[(end-3):end, idx])
+plot!(mean(prediction)[:yields][:, idx])
