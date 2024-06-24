93e4bcef38531cdc03b978599a1e00931ce1dd9c
Preference <econ.preference@gmail.com>
2024-06-24 13:52:18 +0900

    correct errors

--- nomacro.jl
+++ nomacro.jl
@@ -3,8 +3,8 @@ using Pkg, Revise
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
 Pkg.precompile()
-using TermStructureModels, ProgressMeter, Distributions, LinearAlgebra, Distributions, Plots
-using CSV, DataFrames, XLSX, StatsBase, Dates
+using TermStructureModels, ProgressMeter, Distributions, LinearAlgebra, Distributions
+using CSV, DataFrames, XLSX, StatsBase, Dates, JLD2
 
 
 ## Data setting
@@ -14,10 +14,10 @@ date_end = Date("2022-12-01", "yyyy-mm-dd")
 tau_n = [1; 3; 6; 9; collect(12:6:60); collect(72:12:120)]
 medium_tau = collect(36:42)
 
-function data_loading(; date_start, date_end, τₙ)
+function data_loading(; date_start, date_end, tau_n)
 
     ## Yield data
-    raw_yield = XLSX.readdata("LW_monthly.xlsx", "Sheet1", "A293:DQ748") |> x -> [Date.(string.(x[:, 1]), DateFormat("yyyymm")) convert(Matrix{Float64}, x[:, τₙ.+1])] |> x -> DataFrame(x, ["date"; ["Y$i" for i in τₙ]])
+    raw_yield = XLSX.readdata("LW_monthly.xlsx", "Sheet1", "A293:DQ748") |> x -> [Date.(string.(x[:, 1]), DateFormat("yyyymm")) convert(Matrix{Float64}, x[:, tau_n.+1])] |> x -> DataFrame(x, ["date"; ["Y$i" for i in tau_n]])
     yields = raw_yield[findall(x -> x == yearmonth(date_start), yearmonth.(raw_yield[:, 1]))[1]:findall(x -> x == yearmonth(date_end), yearmonth.(raw_yield[:, 1]))[1], :]
     yields = yields[3:end, :]
 
@@ -27,7 +27,7 @@ function data_loading(; date_start, date_end, τₙ)
     return yields
 
 end
-yields = data_loading(; date_start, date_end, τₙ)
+yields = data_loading(; date_start, date_end, tau_n)
 
 ## Setting
 # optimization
@@ -42,12 +42,12 @@ std_kQ_infty = 0.2
 iteration = 5_000
 burnin = 1_000
 
-function estimation(; upper_p, tau_n, medium_τ, iteration, burnin, yields, std_kQ_infty)
+function estimation(; upper_p, tau_n, medium_tau, iteration, burnin, yields, std_kQ_infty)
 
     tuned = JLD2.load("tuned.jld2")["tuned"]
     p = tuned.p
 
-    saved_θ, acceptPrMH = posterior_sampler(Array(yields[upper_p-p+1:end, 2:end]), [], tau_n, [], iteration, tuned; medium_τ, std_kQ_infty)
+    saved_θ, acceptPrMH = posterior_sampler(Array(yields[upper_p-p+1:end, 2:end]), [], tau_n, [], iteration, tuned; medium_tau, std_kQ_infty)
     saved_θ = saved_θ[burnin+1:end]
     iteration = length(saved_θ)
 
@@ -58,7 +58,7 @@ function estimation(; upper_p, tau_n, medium_τ, iteration, burnin, yields, std_
     ineff = ineff_factor(saved_θ)
     JLD2.save("ineff.jld2", "ineff", ineff)
 
-    iter_sub = JLD2.load("ineff.jld2")["ineff"] |> x -> (x[1], x[2], x[3] |> maximum, x[4] |> maximum, x[5] |> maximum, x[6] |> maximum) |> maximum |> ceil |> Int
+    iter_sub = 2
     saved_TP = term_premium(120, tau_n, saved_θ[1:iter_sub:end], Array(yields[upper_p-p+1:end, 2:end]), [])
     JLD2.save("TP.jld2", "TP", saved_TP)
 
@@ -67,7 +67,7 @@ end
 
 ## Do
 
-tuned, opt = tuning_hyperparameter(Array(yields[:, 2:end]), [], τₙ, []; upper_p=1, upper_q, std_kQ_infty, medium_tau)
+tuned, opt = tuning_hyperparameter(Array(yields[:, 2:end]), [], tau_n, []; upper_p=1, upper_q, std_kQ_infty, medium_tau)
 JLD2.save("tuned.jld2", "tuned", tuned, "opt", opt)
 
-estimation(; upper_p, tau_n, medium_τ, iteration, burnin, yields, std_kQ_infty)
+estimation(; upper_p, tau_n, medium_tau, iteration, burnin, yields, std_kQ_infty)
