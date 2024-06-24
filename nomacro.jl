30286106767ef94d3fd8d33e63b6f3b005a9f5f3
Preference <econ.preference@gmail.com>
2024-06-24 15:28:52 +0900

    add full diagnostics

--- nomacro.jl
+++ nomacro.jl
@@ -42,33 +42,141 @@ kappaQ_prior_pr = [truncated(Normal(0.9, 0.05), -1, 1), truncated(Normal(0.9, 0.
 # estimation
 iteration = 5_000
 burnin = 1_000
-
-function estimation(; upper_p, tau_n, medium_tau, iteration, burnin, yields, std_kQ_infty, kappaQ_prior_pr)
-
-    tuned = JLD2.load("tuned.jld2")["tuned"]
-    p = tuned.p
-
-    saved_θ, acceptPrMH = posterior_sampler(Array(yields[upper_p-p+1:end, 2:end]), [], tau_n, [], iteration, tuned; medium_tau, std_kQ_infty, kappaQ_prior_pr)
-    saved_θ = saved_θ[burnin+1:end]
-    iteration = length(saved_θ)
-
-    saved_θ, Pr_stationary = erase_nonstationary_param(saved_θ)
-    iteration = length(saved_θ)
-    JLD2.save("posterior.jld2", "samples", saved_θ, "acceptPrMH", acceptPrMH, "Pr_stationary", Pr_stationary)
-
-    ineff = ineff_factor(saved_θ)
-    JLD2.save("ineff.jld2", "ineff", ineff)
-
-    iter_sub = 2
-    saved_TP = term_premium(120, tau_n, saved_θ[1:iter_sub:end], Array(yields[upper_p-p+1:end, 2:end]), [])
-    JLD2.save("TP.jld2", "TP", saved_TP)
-
-    return []
-end
+iter_sub = 2
 
 ## Do
 
 tuned, opt = tuning_hyperparameter(Array(yields[:, 2:end]), [], tau_n, []; upper_p=1, upper_q, std_kQ_infty, medium_tau, kappaQ_prior_pr)
 JLD2.save("tuned.jld2", "tuned", tuned, "opt", opt)
+p = tuned.p
 
-estimation(; upper_p, tau_n, medium_tau, iteration, burnin, yields, std_kQ_infty, kappaQ_prior_pr)
+saved_θ, acceptPrMH = posterior_sampler(Array(yields[upper_p-p+1:end, 2:end]), [], tau_n, [], iteration, tuned; medium_tau, std_kQ_infty, kappaQ_prior_pr)
+saved_θ = saved_θ[burnin+1:end]
+iteration = length(saved_θ)
+saved_θ, Pr_stationary = erase_nonstationary_param(saved_θ)
+iteration = length(saved_θ)
+ineff = ineff_factor(saved_θ)
+
+saved_TP = term_premium(120, tau_n, saved_θ[1:iter_sub:end], Array(yields[upper_p-p+1:end, 2:end]), [])
+saved_Xθ = latentspace(saved_θ, yields, tau_n)
+reduced_θ = reducedform(saved_θ, yields, macros, tau_n)
+fits = fitted_YieldCurve(tau_n, saved_Xθ)
+
+idx = 18
+plot(mean(fits)[:yields][:, idx])
+plot!(yields[:, idx])
+
+data = [yields macros]
+factors = [PCA(yields, p)[1] macros]
+latentms = [latents macros]
+# Compare observations and fitted variables of the reducedform transition equation
+fitted_samples = Vector{Matrix}(undef, length(reduced_θ))
+@showprogress for iter in eachindex(reduced_θ)
+    KPF = reduced_θ[iter].KPF
+    GPFF = reduced_θ[iter].GPFF
+    OmegaFF = reduced_θ[iter].OmegaFF
+
+    fitted = zeros(size(factors, 1), dP)
+    fitted[1:p, :] = factors[1:p, :]
+    for t in p+1:size(factors, 1)
+        Xs = factors[t-1:-1:t-p, :]' |> vec
+        fitted[t, :] = KPF + GPFF * Xs + rand(MvNormal(zeros(dP), OmegaFF))
+    end
+    fitted_samples[iter] = fitted
+end
+idx = 1
+plot(factors[:, idx])
+plot!(mean(fitted_samples)[:, idx])
+aux = Vector{Float64}(undef, size(factors, 1))
+for t in 1:size(factors, 1)
+    aux[t] = quantile([fitted_samples[i][t, idx] for i in eachindex(fitted_samples)], 0.025)
+end
+plot!(aux)
+aux = Vector{Float64}(undef, size(factors, 1))
+for t in 1:size(factors, 1)
+    aux[t] = quantile([fitted_samples[i][t, idx] for i in eachindex(fitted_samples)], 0.975)
+end
+plot!(aux)
+
+# Compare observations and fitted variables of the latent space transition equation
+fitted_samples = Vector{Matrix}(undef, length(saved_Xθ))
+@showprogress for iter in eachindex(saved_Xθ)
+    KPXF = saved_Xθ[iter].KPXF
+    GPXFXF = saved_Xθ[iter].GPXFXF
+    OmegaXFXF = saved_Xθ[iter].OmegaXFXF + eps() * I(dP) |> x -> 0.5(x + x')
+
+    fitted = zeros(size(latentms, 1), dP)
+    fitted[1:p, :] = latentms[1:p, :]
+    for t in p+1:size(latentms, 1)
+        Xs = latentms[t-1:-1:t-p, :]' |> vec
+        fitted[t, :] = KPXF + GPXFXF * Xs + rand(MvNormal(zeros(dP), OmegaXFXF))
+    end
+    fitted_samples[iter] = fitted
+end
+idx = 4
+plot(latentms[:, idx])
+plot!(mean(fitted_samples)[:, idx])
+aux = Vector{Float64}(undef, size(latentms, 1))
+for t in 1:size(latentms, 1)
+    aux[t] = quantile([fitted_samples[i][t, idx] for i in eachindex(fitted_samples)], 0.025)
+end
+plot!(aux)
+aux = Vector{Float64}(undef, size(latentms, 1))
+for t in 1:size(latentms, 1)
+    aux[t] = quantile([fitted_samples[i][t, idx] for i in eachindex(fitted_samples)], 0.975)
+end
+plot!(aux)
+
+## Diagnostics by the scenario analysis
+predicted_yields = zeros(size(yields, 1), length(tau_n))
+predicted_factors = zeros(size(yields, 1), dP)
+for t = (size(yields, 1)-10):(size(yields, 1)-1)
+    prediction = conditional_forecasts([], 120, 1, saved_θ, yields[1:t, :], macros[1:t, :], tau_n)
+    predicted_yields[t+1, :] = mean(prediction)[:yields][end, :]
+    predicted_factors[t+1, :] = mean(prediction)[:factors][end, :]
+end
+idx = 4
+plot(factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+idx = 18
+plot(yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+
+predicted_yields = zeros(size(yields, 1), length(tau_n))
+predicted_factors = zeros(size(yields, 1), dP)
+for t = (size(yields, 1)-10):(size(yields, 1)-1)
+
+    S = zeros(1, dP - dimQ() + length(tau_n))
+    S[1, 19] = 1.0
+    s = [data[t+1, 19]]
+    scene = Scenario(combinations=deepcopy(S), values=deepcopy(s))
+
+    prediction = conditional_forecasts([scene], 120, 1, saved_θ, yields[1:t, :], macros[1:t, :], tau_n)
+    predicted_yields[t+1, :] = mean(prediction)[:yields][end, :]
+    predicted_factors[t+1, :] = mean(prediction)[:factors][end, :]
+end
+idx = 4
+plot(factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_factors[(size(yields, 1)-9):(size(yields, 1)), idx])
+idx = 18
+plot(yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+plot!(predicted_yields[(size(yields, 1)-9):(size(yields, 1)), idx])
+
+scene = Vector{Scenario}(undef, 4)
+S = zeros(2, dP - dimQ() + length(tau_n))
+s = zeros(2)
+for h = 1:4
+    S[1, 1] = 1.0
+    s[1] = data[size(data, 1)-4+h, 1]
+    S[2, 19] = 1.0
+    s[2] = data[size(data, 1)-4+h, 19]
+    scene[h] = Scenario(combinations=deepcopy(S), values=deepcopy(s))
+end
+prediction = conditional_forecasts(scene, 120, 8, saved_θ, yields[1:size(data, 1)-4, :], macros[1:size(data, 1)-4, :], tau_n)
+plot(mean(prediction)[:TP])
+idx = 4
+plot(factors[(end-3):end, idx])
+plot!(mean(prediction)[:factors][:, idx])
+idx = 18
+plot(yields[(end-3):end, idx])
+plot!(mean(prediction)[:yields][:, idx])
