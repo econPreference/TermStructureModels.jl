de96454011ca7383870b92a8457d5d4b8d9d52fc
Preference <econ.preference@gmail.com>
2025-02-18 17:09:33 +0900

    erase initial values

--- src/inference.jl
+++ src/inference.jl
@@ -232,7 +232,7 @@ function posterior_sampler(yields, macros, tau_n, rho, iteration, tuned::Hyperpa
         x = [kappaQ[1]; diff(kappaQ[1:end])]
         init = [x; kQ_infty; log.(SigmaO)]
         ss = MixedPrecisionRectSearchSpace([0; -1 * ones(length(kappaQ) - 1); -Inf; fill(-Inf, length(tau_n) - dQ)], [1; 0 * ones(length(kappaQ) - 1); Inf; fill(Inf, length(tau_n) - dQ)], zeros(Int64, length(init)))
-        minimizers = bboptimize(x -> -logpost(x), init; SearchSpace=ss) |> best_candidate
+        minimizers = bboptimize(x -> -logpost(x); SearchSpace=ss) |> best_candidate
         x_mode = minimizers[1:dQ]
         x_hess = hessian(x -> -logpost(x), minimizers) |> x -> x[1:dQ, 1:dQ]
         inv_x_hess = inv(x_hess) |> x -> 0.5 * (x + x')
